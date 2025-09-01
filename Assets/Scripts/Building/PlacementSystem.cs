508613ef9bd459ce3b12ecf7b9c1d2494a5cd8f5
LokiGameDev <lokeshwarank007@gmail.com>
2025-09-01 22:54:26 +0530

    Started with grid

--- Assets/Scripts/Building/PlacementSystem.cs
+++ Assets/Scripts/Building/PlacementSystem.cs
@@ -5,23 +5,36 @@ public class PlacementSystem : MonoBehaviour
     #region Variables
 
     [SerializeField]
-    private GameObject placementIndicator;
+    private GameObject placementIndicator, buildingPositionIndicator;
     [SerializeField]
     private InputManager inputManager;
+    [SerializeField]
+    private Grid grid; 
+
     private bool isBuildMode = false;
 
     #endregion
 
+    void Start()
+    {
+        placementIndicator.SetActive(false);
+        buildingPositionIndicator.SetActive(false);
+    }
+
     private void Update()
     {
         if (!isBuildMode) return;
+
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         placementIndicator.transform.position = mousePosition;
+        buildingPositionIndicator.transform.position = grid.CellToWorld(gridPosition);
     }
 
     public void BuildMode(bool state)
     {
         placementIndicator.SetActive(state);
+        buildingPositionIndicator.SetActive(state);
         isBuildMode = state;
     }
 }
